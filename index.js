7f4aa1d60d5923f4c67552804bfa3db04b167a5e
Souhardya Samanta <samantasouhardya14285@gmail.com>
2023-09-11 10:31:59 +0530

    change in port number

--- index.js
+++ index.js
@@ -313,8 +313,8 @@ app.post('/updateItems',isLoggedIn,async(req,res)=>{
 })
 
 
-
-app.listen(3000,()=>{
+const port=process.env.PORT||3000
+app.listen(port,()=>{
     console.log("Listening on port 3000!!..")
 })
 
